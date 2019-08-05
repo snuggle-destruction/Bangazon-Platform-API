1ea21e2678b2673b6f2abb3a9997fcc72acc11b3
deliotclarke <deliotclarke@gmail.com>
2019-08-05 13:03:46 -0500

    installed updates - edited appsettings and luanchsettings to open /customers by default - added sql to get all customers with GET call

--- BangazonAPI/Controllers/CustomersController.cs
+++ BangazonAPI/Controllers/CustomersController.cs
@@ -39,7 +39,7 @@ namespace BangazonAPI.Controllers
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = "Write your SQL statement here to get all customers";
+                    cmd.CommandText = "SELECT * FROM Customer;";
                     SqlDataReader reader = await cmd.ExecuteReaderAsync();
 
                     List<Customer> customers = new List<Customer>();
